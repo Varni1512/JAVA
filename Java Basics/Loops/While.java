0e0f7fb0eb529fc3f718ac36a3a6043d935a9a9d
Varnikumar Patel <varnikumar1512@gmail.com>
2024-08-12 23:30:18 +0530

    Add files via upload

---
+++ Java Basics/Loops/While.java
@@ -0,0 +1,9 @@
+public class While {
+  public static void main(String[] args) {
+    int counter = 0;
+    while (counter < 5) {
+      System.out.println("Hello World");
+      counter++;
+    }
+  }
+}
