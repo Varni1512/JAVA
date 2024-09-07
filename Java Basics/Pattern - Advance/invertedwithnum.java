ea784da6d0825006de63836c73c1500a9c088846
Varnikumar Patel <varnikumar1512@gmail.com>
2024-09-07 15:33:42 +0530

    Add files via upload

---
+++ Java Basics/Pattern - Advance/invertedwithnum.java
@@ -0,0 +1,15 @@
+public class invertedwithnum {
+  public static void inverted_half_pyramid_withnum(int n){
+    for(int i=1; i<=n; i++){
+      //inner - numbers
+      for(int j=1; j<=n-i+1; j++){
+        System.out.print(j+" ");
+      }
+      System.out.println();
+    }
+  }
+  public static void main(String[] args) {
+    inverted_half_pyramid_withnum(5);
+  }
+  
+}
